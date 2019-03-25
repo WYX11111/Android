a2200d1e45e656d8b54d7e4f8e3c540ff64f4ed9
WYX11111 <wyxiao11111@mails.ccnu.edu.cn>
2019-03-25 23:46:11 +0800

    Calculate
    Calculate

---
+++ Calculate/app/src/main/java/com/example/calculate/Calculate.java
@@ -0,0 +1,13 @@
+package com.example.calculate;
+
+public class Calculate {
+    public Calculate(double a,int b){
+        this.a = a;
+        this.b = b;
+    }
+    public double a = 0;
+
+    public int b = 0;
+
+
+}
---
+++ Calculate/app/src/main/java/com/example/calculate/Type.java
@@ -0,0 +1,10 @@
+package com.example.calculate;
+
+public class Type {
+
+    public static final int ADD = 1;
+    public static final int SUB = 3;
+    public static final int X = 5 ;
+    public static final int DIV = 7;
+    public static final int NUM = 9;
+}
